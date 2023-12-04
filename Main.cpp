e31e9274102ad18b19d3ba0743e57fd066c34352
Daniel <39670536+danielkg123@users.noreply.github.com>
2023-12-04 11:57:45 +0700

    Pembuatan menu di main
    buat menu tinggal taruh bagian bagian bisa tambah bisa kurang

--- Main.cpp
+++ Main.cpp
@@ -5,24 +5,85 @@
 using namespace std;
 
 int main() {
-    // membuat maze, input ukuran maze
-    MazeGenerator generator(10, 30);
 
-    // memberitau "aku ingin menjadi admin, aku ingin lihat proses generate maze"
-    // jika ingin generate maze besar, disarankan jangan menggunakan mode admin
-    generator.setAdmin(false);
+    
 
-    // generate maze
+    int width,length;
+    cout << "Input length: ";
+    cin >> length;
+    cout << "Input Width: ";
+    cin >> width;
+
+    //code to start the game
+    MazeGenerator generator(width    , length);
     generator.generateMaze();
     
-    // menampilkan maze dalam bentuk graph
-    cout << "Isi Graph : " << endl;
-    generator.maze.printGraph();
+    cout << "Maze Generated!" << endl;
 
-    // menampilkan tampilan maze
-    cout << endl << "Display Maze : " << endl;
-    string** arrDisplay = generator.getDisplayMaze();
-    generator.displayMaze(arrDisplay);
+
+    int choice;
+
+    // Menu : c14220329
+    do
+    {
+    cout << endl;
+    cout << "Welcome to the Maze Game";
+    cout << endl
+    << " 1 - Display Maze.\n"
+    << " 2 - Start Game.\n"
+    << " 3 - Show Shortest Path. \n"
+    << " 4 - Print Graph.\n"
+    << " 5 - About.\n"
+    << " 6 - Exit.\n"
+    << " Enter your choice : ";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+    {
+        cout << "Display Maze : " << endl;
+        string** arrDisplay = generator.getDisplayMaze();
+        generator.displayMaze(arrDisplay);
+        break;
+    }
+    case 2:
+    {
+        // program start game
+        break;
+    }
+    case 3:
+    {
+        // program show path
+        break;
+    }
+
+    case 4:
+    {
+        cout << "Vertice dan edge map : " << endl;
+        generator.maze.printGraph();
+        break;
+
+    }
+
+    case 5:
+    {
+        cout << "About a Maze game that is created by us";
+        break;
+    }
+
+    case 6:
+    {
+    cout << "End of Program.\n";
+    break;
+    }
+
+    default:
+    cout << "Not a Valid Choice. \n"
+    << "Choose again.\n";
+    break;
+    }
+    } while (choice !=6);
 
     return 0;
-}
+}   
